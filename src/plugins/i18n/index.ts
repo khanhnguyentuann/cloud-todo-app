38edacdddc11f8fb73fda67543797648f5805dda
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2025-07-10 20:29:12 +0900

    feat: implement internationalization support with English, Japanese, and Vietnamese translations

--- src/plugins/i18n/index.ts
+++ src/plugins/i18n/index.ts
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from '@/i18n/en.json';
-import ja from '@/i18n/ja.json';
-import vi from '@/i18n/vi.json';
+import en from '@/plugins/i18n/en.json';
+import ja from '@/plugins/i18n/ja.json';
+import vi from '@/plugins/i18n/vi.json';
 
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
